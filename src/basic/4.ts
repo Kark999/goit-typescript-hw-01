a1b75d4c5545cc13dfc3b7204faa7a0d10bf0a32
Andrey Svyrskyi <akkad1v@gmail.com>
2024-05-03 21:39:04 +0300

    'update'

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -5,7 +5,7 @@ function showMessage(message: string): void {
 function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
-
+// console.log(calc(2, 3));
 function customError(): never {
   throw new Error("Error");
 }
