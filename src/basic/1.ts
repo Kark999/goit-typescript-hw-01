e20251ae2a29f1072ad42956e7692a7865bc1614
Andrey Svyrskyi <akkad1v@gmail.com>
2024-05-03 04:38:15 +0300

    'update'

---
+++ src/basic/1.ts
@@ -0,0 +1,18 @@
+let age: number = 50;
+let username: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+
+// або залишаємо без змін, тому що явно вказані типи змінних.
+
+// const age = 50;
+// const username = "Max";
+// const toggle = true;
+// const empty = null;
+
+type CallbackArg = (a: number) => number;
+const callback: CallbackArg = (a) => {
+  return 100 + a;
+};
+
+console.log(callback(40));
